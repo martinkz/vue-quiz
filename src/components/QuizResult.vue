e7aea74a2bc28bf0dabf7c86bc98652b5da0700c
Martin K <martinkz@users.noreply.github.com>
2022-12-30 10:56:26 +0000

    Add unknown quiz type message

--- src/components/QuizResult.vue
+++ src/components/QuizResult.vue
@@ -5,6 +5,9 @@
   <div v-else-if="resultItem.type === 'scored'">
     <h1>Congratulations, you scored {{ resultItem.result }}!</h1>
   </div>
+  <div v-else>
+    <h1>Unknown quiz type, no result calculated</h1>
+  </div>
 </template>
 
 <script>
