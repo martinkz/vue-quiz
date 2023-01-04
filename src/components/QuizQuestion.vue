dabdcfd799c3231f5d54c6abebd3470ada4eae65
Martin K <martinkz@users.noreply.github.com>
2023-01-04 15:25:33 +0000

    Add next button

--- src/components/QuizQuestion.vue
+++ src/components/QuizQuestion.vue
@@ -3,12 +3,17 @@
 		<h1>{{ questionItem.question }}</h1>
 		<div><img class="question-img" :src="questionItem.image" alt=""></div>
 		<QuizQuestionAnswerBtn v-for="item in questionItem.answers" :key="item" :item="item" />
+		<div class="controls">
+			<button type="button" @click="store.reset()">Next</button>
+		</div>
 	</div>
 </template>
 
 <script setup>
-
 import QuizQuestionAnswerBtn from '@/components/QuizQuestionAnswerBtn.vue';
+import { useQuizStore } from '@/stores/QuizStore';
+
+const store = useQuizStore();
 
 defineProps({
 	questionItem: {
